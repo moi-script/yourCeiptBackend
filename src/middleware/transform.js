942a953bd36122729d27e7635f82a496f9491e6a
lowkeyCodee <rizskplay@gmail.com>
2025-12-18 23:36:49 +0800

    added input sanitize

---
+++ src/middleware/transform.js
@@ -0,0 +1,28 @@
+
+// app.post(
+//   "/orders",
+//   transformOrder,       // normalize input
+//   validateOrderInput,   // express-validator
+//   inspect,              // logs request
+//   gatekeeper,           // authenticate JWT
+//   roleBouncer("user"),  // authorize role
+//   zipper,               // attach extra context
+//   createOrderController // <-- controller
+// );
+
+import sanitize from 'mongo-sanitize';
+
+
+export const transformLogin = (req, res, next) => {
+    if(req.body.email){
+        req.body.email = req.body.email.toLowerCase().trim();
+    }
+    if(req.body.password) {
+        req.body.password = req.body.password.trim()
+    }
+
+    req.body  = sanitize(req.body);
+
+    next();
+}
+
